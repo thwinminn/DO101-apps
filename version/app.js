5cdad328a5b853fc9e17eca20930e75a6250c928
thwinminn <thwinminn@gmail.com>
2020-05-15 23:22:05 +0630

    Updated to Version 3.

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is Openshift of the app.' + '\n';
+    response = 'This is happy weekends!' + '\n';
 
     //send the response to the client
     res.send(response);
