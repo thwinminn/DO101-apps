bd66936eb8b7e0ff144cbb59b1243d931387ee17
thwinminn <thwinminn@gmail.com>
2020-05-16 11:56:21 +0630

    Version1 Saturday Testing

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is happy weekends!' + '\n';
+    response = 'This is Saturday Tesing!' + '\n';
 
     //send the response to the client
     res.send(response);
